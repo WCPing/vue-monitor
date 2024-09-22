0119a619f52d9446f353553aedf99b13182425e5
wuchangping <wu.changping@iwhalecloud.com>
2024-09-22 10:41:56 +0800

    feat: add test coverage

--- packages/monitor/vite.config.ts
+++ packages/monitor/vite.config.ts
@@ -7,4 +7,9 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
+    test: {
+        coverage: {
+            provider: 'v8', // or 'v8'
+        },
+    },
 })
